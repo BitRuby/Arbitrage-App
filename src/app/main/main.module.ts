8a147f16dbd29d6374ede7788341e783ae0b2eb7
Michał Zwierzyński <firewallosiem@gmail.com>
2018-11-26 13:59:47 +0100

    Added scrollable overlay

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { MainComponent } from './main.component';
-import { CommonModule } from '@angular/common';
-import { MatListModule, MatTableModule, MatGridListModule, MatSortModule, MatPaginatorModule } from '@angular/material';
-import { MainRoutingModule } from './main-routing.module';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
 import { TableNumRowsComponent } from './table-num-rows/table-num-rows.component';
-import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 import { OrderBookUsdBtcComponent } from './tables/order-book/asks/order-book-usd-btc/order-book-usd-btc.component';
 import { OrderBookUsdLtcComponent } from './tables/order-book/asks/order-book-usd-ltc/order-book-usd-ltc.component';
 import { OrderBookUsdEthComponent } from './tables/order-book/asks/order-book-usd-eth/order-book-usd-eth.component';
@@ -14,12 +11,18 @@ import { OrderBookUsdAdaComponent } from './tables/order-book/asks/order-book-us
 import { OrderBookUsdUsdtComponent } from './tables/order-book/asks/order-book-usd-usdt/order-book-usd-usdt.component';
 import { OrderBookUsdXrpComponent } from './tables/order-book/asks/order-book-usd-xrp/order-book-usd-xrp.component';
 import { OrderBookUsdZecComponent } from './tables/order-book/asks/order-book-usd-zec/order-book-usd-zec.component';
+import { CommonModule } from '@angular/common';
+import { MatListModule, MatTableModule, MatGridListModule, MatSortModule, MatPaginatorModule } from '@angular/material';
+import { MainRoutingModule } from './main-routing.module';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+
 
 @NgModule({
   declarations: [
     MainComponent,
-    HeaderComponent,
     FooterComponent,
+    HeaderComponent,
+    MenuComponent,
     TableNumRowsComponent,
     OrderBookUsdBtcComponent,
     OrderBookUsdLtcComponent,
---
+++ src/app/main/menu/menu.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { MenuService } from './menu.service';
+
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  @HostBinding('class.slideDown')
+  isOpen = false;
+
+  constructor(private menuService: MenuService) { }
+
+  ngOnInit() {
+    this.menuService.change.subscribe(isOpen => {
+      this.isOpen = isOpen;
+    });
+  }
+
+}
