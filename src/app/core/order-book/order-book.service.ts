8799fa06fca092ae6edf250c63caab1a79ce3d18
Michał Zwierzyński <firewallosiem@gmail.com>
2018-12-29 20:59:18 +0100

    Updated styles, removed unused methods

--- src/app/core/order-book/order-book.service.ts
+++ src/app/core/order-book/order-book.service.ts
@@ -11,22 +11,6 @@ export class OrderBookService {
 
   constructor(private httpClient: HttpClient) {
   }
-  getOrderBookBittrex(c1: string, c2: string): Observable<Order> {
-    const url = `http://localhost:8000/api/bittrex/orderbook/${c1}/${c2}`;
-    return this.httpClient.get<Order>(url);
-  }
-  getOrderBookBitfinex(c1: string, c2: string): Observable<Order> {
-    const url = `http://localhost:8000/api/bitfinex/orderbook/${c1}/${c2}`;
-    return this.httpClient.get<Order>(url);
-  }
-  getOrderBookCoinbase(c1: string, c2: string): Observable<Order> {
-    const url = `http://localhost:8000/api/coinbase/orderbook/${c1}/${c2}`;
-    return this.httpClient.get<Order>(url);
-  }
-  getOrderBookKraken(c1: string, c2: string): Observable<Order> {
-    const url = `http://localhost:8000/api/kraken/orderbook/${c1}/${c2}`;
-    return this.httpClient.get<Order>(url);
-  }
   getOrderBook(id: Number): Observable<OrderBookData[]> {
     const url = `http://localhost:8000/api/orderbook/${id}`;
     return this.httpClient.get<OrderBookData[]>(url);
