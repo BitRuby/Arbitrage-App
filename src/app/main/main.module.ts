ead5062eeb08dd1bdcd6af61e6b66704bca1f720
Michał Zwierzyński <firewallosiem@gmail.com>
2018-12-29 18:29:40 +0100

    Updated directories

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -31,14 +31,14 @@ import { NgModule } from '@angular/core';
 import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 import { TableNumRowsComponent } from './table-num-rows/table-num-rows.component';
 import { FormsModule } from '@angular/forms';
-import { BookUsdtBtcComponent } from './tables/order-book/asks/book-usdt-btc/book-usdt-btc.component';
-import { BookUsdtEthComponent } from './tables/order-book/asks/book-usdt-eth/book-usdt-eth.component';
-import { BookUsdtLtcComponent } from './tables/order-book/asks/book-usdt-ltc/book-usdt-ltc.component';
-import { BookUsdtXrpComponent } from './tables/order-book/asks/book-usdt-xrp/book-usdt-xrp.component';
-import { BookUsdtZecComponent } from './tables/order-book/asks/book-usdt-zec/book-usdt-zec.component';
-import { BookUsdtAdaComponent } from './tables/order-book/asks/book-usdt-ada/book-usdt-ada.component';
-import { BookUsdtXmrComponent } from './tables/order-book/asks/book-usdt-xmr/book-usdt-xmr.component';
-import { BookUsdtBchComponent } from './tables/order-book/asks/book-usdt-bch/book-usdt-bch.component';
+import { BookUsdtBtcComponent } from './tables/order-book/book-usdt-btc/book-usdt-btc.component';
+import { BookUsdtEthComponent } from './tables/order-book/book-usdt-eth/book-usdt-eth.component';
+import { BookUsdtLtcComponent } from './tables/order-book/book-usdt-ltc/book-usdt-ltc.component';
+import { BookUsdtXrpComponent } from './tables/order-book/book-usdt-xrp/book-usdt-xrp.component';
+import { BookUsdtZecComponent } from './tables/order-book/book-usdt-zec/book-usdt-zec.component';
+import { BookUsdtAdaComponent } from './tables/order-book/book-usdt-ada/book-usdt-ada.component';
+import { BookUsdtXmrComponent } from './tables/order-book/book-usdt-xmr/book-usdt-xmr.component';
+import { BookUsdtBchComponent } from './tables/order-book/book-usdt-bch/book-usdt-bch.component';
 
 
 
